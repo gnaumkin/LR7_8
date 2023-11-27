076da2a2b348881033ad1cc0c470e66c002c892c
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-11-28 00:38:00 +0300

    Add files via upload

---
+++ z13.pas
@@ -0,0 +1,22 @@
+﻿program z13;
+var
+  instr: String;
+  i: Integer;
+  pravd: Boolean;
+begin
+  write('Введите строку: ');
+  readln(instr);
+  pravd := True;
+  for i := 1 to Length(instr) do
+  begin
+    if not (instr[i] in ['a', 'b', 'c']) then
+    begin
+      pravd := False;
+      Break;
+    end;
+  end;
+  if pravd then
+    writeln('Строка содержит только символы a,b,c.')
+  else
+    writeln('Строка содержит другие символы помимо a,b,c.');
+end.
---
+++ z3.pas
@@ -0,0 +1,16 @@
+﻿program z3;
+var
+  str: string;
+  len: integer;
+begin
+  writeln('Введите строку:');
+  readln(str);
+  len := length(str);
+  writeln('Первый символ: ', str[1]);
+  writeln('Последний символ: ', str[len]);
+  if len mod 2 <> 0 then
+    writeln('Средний символ: ', str[(len + 1) div 2])
+  else
+    writeln('Длина строки четная, нет среднего символа.');
+  readln;
+end.
