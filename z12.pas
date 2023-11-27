d61c5fbf473a35757a2edd9568249723ad7ffbc5
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-11-27 21:18:58 +0300

    Add files via upload

---
+++ z12.pas
@@ -0,0 +1,12 @@
+﻿program z12;
+var s: string;
+i, n, len: integer;
+begin
+ readln(s);
+ len := length(s);
+  n := 0;
+  for i:=1 to len do 
+    if (ord(s[i]) > 47) and (ord(s[i]) < 58) then 
+      n := n + 1;
+ writeln(n);
+end.
