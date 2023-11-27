0dc51a6703fea1c684abf0b63a9e1dfe81f77914
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-11-28 00:51:38 +0300

    Add files via upload

---
+++ z11.pas
@@ -0,0 +1,19 @@
+﻿program z11;
+var
+  instr: string;
+begin
+  write('Введите строку: ');
+  readln(instr);
+  if length(instr) > 10 then
+  begin
+    instr := copy(instr, 1, 6);
+  end
+  else
+  begin
+    while length(instr) < 12 do
+    begin
+      instr := instr + 'o';
+    end;
+  end;
+  writeln('Результат: ', instr);
+end.
---
+++ z4.pas
@@ -0,0 +1,22 @@
+﻿program z4;
+var
+  str: string;
+  len,i: integer;
+begin
+  writeln('Введите строку:');
+  readln(str);
+  len := length(str);
+  if len > 5 then
+  begin
+    writeln('Первые три символа: ', copy(str, 1, 3));
+    writeln('Последние три символа: ', copy(str, len - 2, 3));
+  end
+  else
+  begin
+    writeln('Первый символ повторен ', len, ' раз:');
+    for i := 1 to len do
+      write(str[1]);
+    writeln;
+  end;
+  readln;
+end.
