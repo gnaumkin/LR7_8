8fcbcab550f9c99d1d7f85a4e3fd833a99f9abfc
gnaumkin <144417191+gnaumkin@users.noreply.github.com>
2023-11-28 10:53:10 +0300

    Update z12.pas

--- z12.pas
+++ z12.pas
@@ -1,12 +1,12 @@
-﻿program z12;
+program z12;
 var s: string;
-i, n, len: integer;
+i, n: integer;
 begin
  readln(s);
- len := length(s);
   n := 0;
-  for i:=1 to len do 
-    if (ord(s[i]) > 47) and (ord(s[i]) < 58) then 
-      n := n + 1;
+  for i:=1 to length(s) do
+    begin 
+    if (s[i] >= '0') and (s[i] <= '9') then inc(n); 
+    end;
  writeln(n);
 end.
